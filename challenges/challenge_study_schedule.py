ec20fd763758a36f5c6af33b04ba8bfdbd189ab4
Danillo Santos <dnashy@hotmail.com>
2023-10-24 17:50:01 -0300

    initial commit + feat: solve challenge one

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    :param permanence_period: list of tuples (start, end)
+    :param target_time: int
+    :return: int
+    """
+    counter = 0
+    if target_time is None:
+        return None
+    for student_period in permanence_period:
+        if not isinstance(student_period[0], int) or not isinstance(
+            student_period[1], int
+        ):
+            return None
+        elif (
+            target_time >= student_period[0]
+            and target_time <= student_period[1]
+        ):
+            counter += 1
+    return counter
