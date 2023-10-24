12fe1cf86e7b05d8f767cd56ec6e4331f474899c
Danillo Santos <dnashy@hotmail.com>
2023-10-24 19:39:09 -0300

    feat: add structure to the function is_anagram/ and pause the development

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,6 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+    first_string_sorted = ''
+    second_string_sorted = ''
+    anagram = False
+    return ((first_string_sorted, second_string_sorted), anagram)
