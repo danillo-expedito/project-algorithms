bf7ef9aacb4ccd104836e4fbe33dee2363d0f8bd
Danillo Santos <dnashy@hotmail.com>
2023-10-24 19:30:58 -0300

    feat: add new function that verifys palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # Base case (kinda)
+    if len(word) == 0:
+        return False
+    # Compare the first and last index
+    elif low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    # recursive case that moves the indexes to compare the next letters
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
