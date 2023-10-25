7a9754a76f8a0a29c30ac574468d6014c5fb808c
Danillo Santos <dnashy@hotmail.com>
2023-10-25 12:56:06 -0300

    feat: add the is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,18 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # Verify if the word is empty
+    if len(word) == 0:
+        return False
+    # Declare the indexes
+    low_index = 0
+    high_index = len(word) - 1
+
+    # Compare the first and last index and
+    # move the indexes to compare the next letters
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    # If the word is a palindrome, return True
+    return True
