998706526c6850c9a77f3b95244aedd424860a65
Danillo Santos <dnashy@hotmail.com>
2023-10-25 14:22:27 -0300

    fix: change the is_palindrome_iterative function trying to reduce its complexity

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,23 @@
+# def is_palindrome_iterative(word):
+#     """Faça o código aqui."""
+#     # Verify if the word is empty
+#     if len(word) == 0:
+#         return False
+#     # Declare the indexes
+#     low_index = 0
+#     high_index = len(word) - 1
+
+#     # Compare the first and last index and
+#     # move the indexes to compare the next letters
+#     while low_index < high_index:
+#         if word[low_index] != word[high_index]:
+#             return False
+#         low_index += 1
+#         high_index -= 1
+#     # If the word is a palindrome, return True
+#     return True
+
+
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
     # Verify if the word is empty
@@ -5,14 +25,12 @@ def is_palindrome_iterative(word):
         return False
     # Declare the indexes
     low_index = 0
+    mid = len(word) // 2
     high_index = len(word) - 1
 
-    # Compare the first and last index and
-    # move the indexes to compare the next letters
-    while low_index < high_index:
+    for _ in range(mid):
         if word[low_index] != word[high_index]:
             return False
         low_index += 1
         high_index -= 1
-    # If the word is a palindrome, return True
     return True
