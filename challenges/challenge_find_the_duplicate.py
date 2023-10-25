d4714ee4f53dd4b01ec89245ad6d5ddf03a509d9
Danillo Santos <dnashy@hotmail.com>
2023-10-25 12:37:48 -0300

    feat: add find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    # Verify if nums is a list, if it is not, return False
+    if nums is None or isinstance(nums, str) or len(nums) == 1:
+        return False
+
+    # Sort the list and create a set to store the numbers
+    nums.sort()
+    duplicate = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:  # Verify if the list has only
+            return False                         # positive integers
+        if num in duplicate:  # Verify if the number is already in the set
+            return num        # If it is, return the number
+        duplicate.add(num)    # If it is not, add the number to the set
+    return False
