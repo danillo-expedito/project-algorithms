dce2fe188aa98bf222738d900189f7b897c021cc
Danillo Santos <dnashy@hotmail.com>
2023-10-27 18:21:53 -0300

    feat: add new solution from the palindrome_iterative function using just one line

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -18,19 +18,23 @@
 #     return True
 
 
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    # Verify if the word is empty
-    if len(word) == 0:
-        return False
-    # Declare the indexes
-    low_index = 0
-    mid = len(word) // 2
-    high_index = len(word) - 1
+# def is_palindrome_iterative(word):
+#     """Faça o código aqui."""
+#     # Verify if the word is empty
+#     if len(word) == 0:
+#         return False
+#     # Declare the indexes
+#     low_index = 0
+#     mid = len(word) // 2
+#     high_index = len(word) - 1
 
-    for _ in range(mid):
-        if word[low_index] != word[high_index]:
-            return False
-        low_index += 1
-        high_index -= 1
-    return True
+#     for _ in range(mid):
+#         if word[low_index] != word[high_index]:
+#             return False
+#         low_index += 1
+#         high_index -= 1
+#     return True
+
+# Solution using only one line, a challenge from the live lecture CS 3.2
+def is_palindrome_iterative(word):
+    return word == word[::-1] if len(word) > 0 else False
